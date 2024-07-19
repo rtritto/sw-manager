d9be82553e0d6ee2d66fd5ca0c705c7ce97bbc7f
rtritto <ariele.1994@gmail.com>
2024-07-19 16:22:02 +0200

    Clean App

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,17 +3,7 @@ import Home from '../components/Home'
 
 
 const App: Component = () => {
-  return (
-    <div>
-      <h1>Solid + Vite + TypeScript</h1>
-
-      <p class="read-the-docs">
-        Click on the Solid, Vite and TypeScript logos to learn more
-      </p>
-
-      <Home />
-    </div>
-  )
+  return <Home />
 }
 
 export default App
